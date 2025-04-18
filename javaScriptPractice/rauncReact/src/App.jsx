fe89de51c9f7d6318dd59f02cc70644bcbeff24a
Rauno <rauno.rander@mail.ee>
2025-04-18 12:55:47 +0300

    React

--- javaScriptPractice/rauncReact/src/App.jsx
+++ javaScriptPractice/rauncReact/src/App.jsx
@@ -1,10 +1,10 @@
-import ChildrenProps from "./components/ChildrenProps/";
+import CustomerServiceSurvey from "./components/UnControlledInputWindowObject"; // Removed trailing slash
 
 function App() {
   return (
     <div>
       <h1>Hello!</h1>
-      <ChildrenProps /> {/* PascalCase confirmed */}
+      <CustomerServiceSurvey /> {/* Changed to match imported name */}
     </div>
   );
 }
